cec8c7b06ef00d2883ff58eaaacf674fe8d4a1f6
Ben Kintaudi <bkintaudi@hotmail.com>
2018-10-19 20:18:40 -0500

    Card Components

---
+++ new 7.js
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="all.min.css">
+    <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+<style>
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 40%;
+}
+
+.price{
+color: orange;
+}
+
+.card:hover {
+    box-shadow: 0 6px 12px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+    padding: 20px 20px;
+	width:500px;
+	height:1000px ;
+}
+
+color
+{}
+</style>
+</head>
+<body>
+
+<div class="container">
+   <div class="row">
+      <div class="col-md-3 col-sm-6">
+
+		    <div class="card-block">
+			   <img src="tryit.jpg" height="100px"  width=" 200px" alt="Barcelona" class="img-fluid">
+			   
+			   <div class="row">
+			   <div  class="col-md-8 col-sm-8">
+					<span> Chipotle Turkey<br>panini </span>
+			   </div>
+			   <div class="col-md-4 col-sm-4">
+			   
+                     <a href="#"><span class="glyphicon glyphicon-heart"></span></a> Like
+			   </div>
+			   </div>
+	              
+               <div class="row">
+				  <div  class="col-md-9 col-sm-9">
+					<span> description </span>
+			      </div>
+				  <div class="col-md-3 col-sm-3">
+			   
+                     <p class="price">$0.00</p>
+			      </div>
+				  
+			   <button style="margin-top: 10px" href="#" class="btn btn-success ">Add to chart</button>
+		    </div>
+        
+	  </div>
+
+	  
+   </div>
+</div>  
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> 
+
+
+</body>
+</html> 

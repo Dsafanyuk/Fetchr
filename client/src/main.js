a65af9ca4520b1104a2f0e09cc59da86c10130b0
Kesney <kesney095@gmail.com>
2018-11-01 21:48:25 -0700

    Added Vue Cookie. Fixed some design issues

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue       from 'vue'
 import Vuetify   from 'vuetify'
 import App       from './Components/App.vue'
 import VueRouter from 'vue-router'
+import VueCookies from 'vue-cookies'
 import Home      from './Components/Home.vue'
 import Login     from './Components/Login.vue'
 import Register  from './Components/Register.vue'
@@ -10,6 +11,7 @@ import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(VueRouter);
 Vue.use(Vuetify);
+Vue.use(VueCookies);
 /*----------------------- Routes Declaration -----------------*/
 const routes = [
   {path : '/',         component : Home      },

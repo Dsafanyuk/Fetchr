315d2fd018c7174fe72ebcd66fcd59403456cb3b
Dave Safanyuk <dsafanyuk@gmail.com>
2018-11-06 11:43:55 -0600

    Changed product cards

--- client/src/Components/mini-components/LandingCard.vue
+++ client/src/Components/mini-components/LandingCard.vue
@@ -1,54 +1,59 @@
 <template>
-  <div id="landing_card" class=" card_prop col-md-3 col-xs-2">
-     <div class="thumb">
-          <ProductInfo>
+<div class="col-lg-3 m8 s12 offset-m2">
+    <div class="product-card">
+        <div class="card  z-depth-4">
+            <div class="card-image">
+                <a href="#" class="btn-floating btn-large price waves-effect waves-light brown darken-3">{{product.price}}</a>
+                <img :src=product.product_url :alt=product.product_name>
+                <span class="card-title text-center" style="position: absolute;
+                                                            top: 50%;
+                                                            left: 50%;
+                                                            transform: translate(-50%, -50%);">
+                <span>{{product.product_name}}</span></span>
+            </div>
+            <ul class="card-action-buttons">
 
-           </ProductInfo>
-     </div>
-     <div class="details">
-        <div class="row">
-  	     <div class=" col-md-8 col-xs-9">
-              <span>{{product.product_name}}</span>
-  		 </div>
-  		 <div class=" col-md-4 col-xs-3 links">
-              <a href="#"><i class="far fa-heart fa_la"></i></a>
-           </div>
-  		 <br><br>
-  	  </div>
-        <div class="bottom-details">
-  	     <div class="price">{{product.price}}</div>
-  	     <div class="links">
-  	        <a href=""><i class="fa fa-shopping-cart fa-md"></i></a>
-  	     </div>
+                <li><a class="btn-floating waves-effect waves-light red accent-2"><i class="material-icons like">favorite_border</i></a>
+                </li>
+                <li><a id="buy" class="btn-floating waves-effect waves-light blue"><i class="material-icons buy">add_shopping_cart</i></a>
+                </li>
+            </ul>
+            <div class="card-content">
+                <div class="row">
+                    <div class="col s12">
+                        <p>
+                            <strong>Description:</strong> <br />
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare auctor metus vel mollis.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
         </div>
-     </div>
-
-  </div>
+    </div>
+</div>
 </template>
 
 <script>
-import ProductInfo from './ProductInfo.vue';
-
 export default {
   props: {
-  product: {
-    product_id: String,
-    product_name: String,
-    price: Number,
-    category: String,
-    product_url: String,
+    product: {
+      product_id: String,
+      product_name: String,
+      price: Number,
+      category: String,
+      product_url: String,
+    },
   },
-},
   data() {
-    return {
-    };
+    return {};
   },
-  components: {
-    ProductInfo: ProductInfo,
-  },
+  components: {},
 };
 </script>
 
 <style lang="css">
 @import '../custom_css/landing_card.scss';
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css');
 </style>
--- client/src/Components/mini-components/LandingHeader.vue
+++ client/src/Components/mini-components/LandingHeader.vue
@@ -8,7 +8,6 @@
         </ul>
         <form class="col-md-4 form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2 " size="40" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
         <div class="col-md-4 icons_div">
           <i class="far fa-user"></i>
@@ -19,16 +18,13 @@
 </template>
 <script>
 export default {
-
-  data () {
-    name: "LandingHeader"
-    return {
-      
-    }
-  }
-}
+  data() {
+    name: 'LandingHeader';
+    return {};
+  },
+};
 </script>
 
 <style lang="css">
-@import '../custom_css/landing.scss'
+@import '../custom_css/landing.scss';
 </style>

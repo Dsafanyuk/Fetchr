aaefbe52bbda08b5dec68ebf1fb0c677d4b92bb8
James Limantara <james.limantara@students.pcci.edu>
2018-11-10 23:19:09 -0600

    push

--- client/src/Components/Login.vue
+++ client/src/Components/Login.vue
@@ -1,18 +1,68 @@
 <template>
+  <div class="container register">
+    <div class="col-md-12 mx-auto register-right">
+      <div class="row register-form">
+        <div class="col-md-6 mx-auto">
+          <img class="center_img" width="300px" height="300px " src="./images/logo.png" />
+        </div>
 
+        <div class="col-md-6 mx-auto">
+
+          <div class="form-group">
+            <h4 class="text-center form_h"> Login in on our Platform </h4>
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" v-model="cEmail" placeholder="Email" value="" />
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" v-model="cPassword" required placeholder="Password *" value="" />
+          </div>
+          <div class="form-group text-center">
+            <input type="submit" @click="loginCustomer" class="btnRegister" value="Login" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
+  import axios from 'axios'
 
-  data () {
-    name:
-    return {
+  export default {
+    //******************************************* Component Name  ********************************/
+    name: "login",
+    data() {
+      return {
+        cEmail: '',
+        cPassword: '',
+      }
+    },
+    methods: {
+      loginCustomer(e) {
+        let api_url = "http://localhost:3000/api/users/login";
 
+        if (this.cEmail && this.cPassword) {
+          axios.post(api_url, {
+            email_address: this.cEmail,
+            password: this.cPassword
+          })
+            .then(function (response) {
+              if (response.status == 200) {
+                console.log('logged in');
+                this.$router.push('/');
+              }
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        } else {
+          console.log('at least 1 null field');
+        }
+      }
     }
   }
-}
 </script>
 
 <style lang="css">
-@import 'custom_css/registration.scss'
+  @import 'custom_css/registration.scss'
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,26 +1,26 @@
-import Vue       from 'vue'
-import Vuetify   from 'vuetify'
-import App       from './Components/App.vue'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './Components/App.vue'
 import VueRouter from 'vue-router'
-import Home      from './Components/Home.vue'
-import Login     from './Components/Login.vue'
-import Register  from './Components/Register.vue'
-import Landing   from './Components/Landing.vue'
+import Home from './Components/Home.vue'
+import Login from './Components/Login.vue'
+import Register from './Components/Register.vue'
+import Landing from './Components/Landing.vue'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(VueRouter);
 Vue.use(Vuetify);
 /*----------------------- Routes Declaration -----------------*/
 const routes = [
-  {path : '/',         component : Home      },
-  {path : '/login',    component : Login     },
-  {path : '/register', component : Register  },
-  {path : '/dashboard',  component : Landing }
+  { path: '/', component: Home },
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '/dashboard', component: Landing }
 ];
 
 const router = new VueRouter({
   routes,
-  mode : 'history'
+  mode: 'history',
 });
 
 

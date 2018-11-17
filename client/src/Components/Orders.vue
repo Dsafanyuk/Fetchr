4ff0d5076fd233787dc9ac03e48688125817fb5e
calebmurrays <caleb.murray@students.pcci.edu>
2018-11-16 20:24:02 -0600

    order summary

--- client/src/Components/Orders.vue
+++ client/src/Components/Orders.vue
@@ -20,7 +20,34 @@
                     <td>{{order.delivery_status}}</td>
                     <td>${{order.order_total.toFixed(2)}}</td>
                     <td>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="button">View</button>
+                        <v-dialog width="500">
+                            <button slot="activator" class="btn btn-outline-success my-2 my-sm-0" @click="showModal = true" type="button">View</button>
+                                <v-card>
+                                <v-card-title
+                                    class="headline grey lighten-2"
+                                    primary-title
+                                >
+                                    Order Summary
+                                </v-card-title>
+                            
+                                <v-card-text>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </v-card-text>
+                            
+                                <v-divider></v-divider>
+                            
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn
+                                    color="primary"
+                                    flat
+                                    @click="dialog = false"
+                                    >
+                                    I accept
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
                     </td>
                 </tr>
             </tbody>
@@ -40,7 +67,7 @@ export default {
   data () {
     name: 
     return {
-        orders: {}
+        orders: {},
     };
   },
   mounted: function() {
@@ -50,7 +77,7 @@ export default {
   },
   components: {
     LandingHeader: LandingHeader,
-    LandingFooter: LandingFooter
+    LandingFooter: LandingFooter,
   },
   methods: {
     fixDate: function(date) {

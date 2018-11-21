acfd720773bdcc78d7acae5eabd676f2b911c3b1
Kesney <kesney095@gmail.com>
2018-11-20 16:50:59 -0800

    --

--- client/src/Components/Landing.vue
+++ client/src/Components/Landing.vue
@@ -1,7 +1,7 @@
 <template>
 <v-app>
-    <LandingHeader> </LandingHeader>
-    <ShoppingCart v-if = "seen" ></ShoppingCart>
+
+
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">

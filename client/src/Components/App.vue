6bc52a6e0d10643d13556c827e6dc43e58b85798
Dave Safanyuk <dsafanyuk@gmail.com>
2018-11-29 18:27:30 -0600

    small env changes may revert later

---
+++ client/src/Components/App.vue
@@ -0,0 +1,17 @@
+<template >
+  <div id="home">hello
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>

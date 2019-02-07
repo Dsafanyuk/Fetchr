a7999c8f85a4a76c64b74a98e5d252c2e6d7a210
Kesney <kesney095@gmail.com>
2019-02-06 22:55:24 -0800

    Dashboard Component added

--- client/src/Components/Dashboard.vue
+++ client/src/Components/Dashboard.vue
@@ -1,28 +1,28 @@
 <template>
-  <body>
-  <DashboardHeader> </DashboardHeader>
 
-</body>
+  <body>
+    <DashboardHeader> </DashboardHeader>
+
+  </body>
 
 
 
 </template>
 
 <script>
-import DashboardHeader from "./mini-components/DashboardHeader.vue";
+  import DashboardHeader from "./mini-components/DashboardHeader.vue";
 
-export default {
-  data() {
-    return {
-    };
-  },
-  components: {
-    DashboardHeader: DashboardHeader,
-  },
-  methods: {
+  export default {
+    data() {
+      return {};
+    },
+    components: {
+      DashboardHeader: DashboardHeader,
+    },
+    methods: {
 
-  }
-};
+    }
+  };
 </script>
 
 <style scoped="true">

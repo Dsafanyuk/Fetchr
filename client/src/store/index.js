4e1415c4ae38b098c3072833139d036496bcbdb7
Dave Safanyuk <dsafanyuk@gmail.com>
2019-02-11 09:09:36 -0600

    Persisted vuex state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import cart from './modules/cart'
 import wallet from './modules/wallet'
 Vue.use(Vuex)
@@ -7,6 +8,7 @@ Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
+    plugins: [createPersistedState()],
     modules: {
         cart,
         wallet

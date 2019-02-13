a6c2a1c6171d3a4e112efd8acdbd663b603b0ed1
Kesney <kesney095@gmail.com>
2019-02-12 18:53:31 -0800

    Chart Functionality

--- api/courier/courierController.js
+++ api/courier/courierController.js
@@ -47,8 +47,29 @@ function acceptedOrders(req, res) {
       console.log(err);
     });
 }
+// Get total available Orders
+function countAvailableOrder(req, res ){
+ knex.raw ('SELECT COUNT(*) as count_av FROM orders WHERE courier_id is null')
+.then((orders) => {
+    res.send(orders);
+})
+}
+// Get delivered Orders
+function countDelivered(req, res ){
 
-// POST 
+  knex.raw ('SELECT COUNT(*) as count_d FROM orders WHERE courier_id = '+ req.params.user_id +
+           ' AND delivery_status = \'pending\' ')
+.then((count_d) => {
+     res.send(count_d);
+ })
+}
+function getRevenue(req, res ){
+  knex.raw ('SELECT SUM(order_total) as revenue FROM orders WHERE courier_id = '+ req.params.user_id)
+.then((revenue) => {
+     res.send(revenue);
+ })
+}
+// POST
 function acceptOrder(req, res) {
   knex('orders')
     .whereNull('courier_id')
@@ -72,30 +93,11 @@ function acceptOrder(req, res) {
       console.log(err);
     });
 }
-
-// POST
-function deliverOrder(req, res) {
-  knex('orders')
-    .where('order_id', req.params.order_id)
-    .update('delivery_status', 'delivered')
-    .then((rows) => {
-      if (rows == 1) {
-        res.send("success");
-      }
-      else {
-        res.send("fail");
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: `${err}`,
-      }); // FOR DEBUGGING ONLY, dont send exact message in prod
-      console.log(err);
-    });
-}
 module.exports = {
   acceptOrder,
   acceptedOrders,
   showCourierOrders,
-  deliverOrder,
+  countAvailableOrder,
+  countDelivered,
+  getRevenue,
 };

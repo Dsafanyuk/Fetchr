039294782da8bbe798deaa577848d42487b351e7
calebmurrays <caleb.murray@students.pcci.edu>
2019-02-15 21:29:39 -0600

    cant keep adding and can type other amount

--- client/src/Components/mini-components/Wallet.vue
+++ client/src/Components/mini-components/Wallet.vue
@@ -20,13 +20,25 @@
             <v-item v-for="n in refillAmounts" :key="n">
               <v-chip slot-scope="{ active }" :selected="active" @click="selectAmount(n)">${{ n }}</v-chip>
             </v-item>
+            <v-item>
+              <input
+                v-if="otherChosen" 
+                autofocus
+                slot-scope="{ active }" 
+                v-on:keypress="isNumber" 
+                :selected="active" 
+                placeholder="$" 
+                v-model="selectedAmount" 
+                class="otherAmount">
+              <v-chip v-else slot-scope="{ active }" :selected="active" @click="otherClick">other</v-chip>
+            </v-item>
           </v-item-group>
         </v-card-actions>
         <div v-if="transactionIsProcessing">
           <v-progress-linear :indeterminate="true" height="15" color="success"></v-progress-linear>
         </div>
-        <div v-else class="text-xs-center" @click="addToWallet">
-          <v-btn :disabled="validAmountChosen" round color="success">Refill &nbsp; &nbsp;
+        <div v-else class="text-xs-center">
+          <v-btn :disabled="validAmountChosen" round color="success" @click="addToWallet">Refill &nbsp; &nbsp;
             <v-icon>fas fa-money-bill-wave</v-icon>
           </v-btn>
         </div>
@@ -39,6 +51,7 @@
 import browserCookies from "browser-cookies";
 import axios from "axios";
 import { mapGetters } from "vuex";
+import toasted from "vue-toasted";
 
 const api = axios.create();
 
@@ -50,19 +63,38 @@ export default {
     return {
       refillAmounts: [5, 10, 15, 25],
       selectedAmount: null,
-      transactionIsProcessing: false
+      transactionIsProcessing: false,
+      otherChosen: false,
     };
   },
   created: function() {
     this.$store.dispatch("wallet/getWalletBalance");
   },
   methods: {
+    // so they cant type letters into the other amount field if we care about that kind of thing
+    // from stack overflow
+    isNumber: function(evt) {
+      evt = (evt) ? evt : window.event;
+      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+        evt.preventDefault();;
+      } else {
+        return true;
+      }
+    },
+    otherClick() {
+      this.selectedAmount = null;
+      this.otherChosen = true;
+      //set focus
+      setTimeout(() => {document.getElementsByClassName("otherAmount")[0].focus()}, 20)
+    },
     selectAmount(n) {
       this.selectedAmount = n;
+      this.otherChosen = false;
     },
     addToWallet() {
       this.transactionIsProcessing = true;
-      if (this.refillAmounts.includes(this.selectedAmount)) {
+      if (parseFloat(this.selectedAmount) + parseFloat(this.walletBalance) < 1000) {
         console.log("true");
         api
           .post("/api/users/" + browserCookies.get("user_id") + "/wallet", {
@@ -72,10 +104,10 @@ export default {
             console.log(response.status);
             if (response.status == 200) {
               console.log("transaction successful");
-
               this.$store.dispatch("wallet/getWalletBalance");
             }
             this.transactionIsProcessing = false;
+            this.selectAmount(null);
           })
           .catch(error => {
             if (error.response) {
@@ -89,6 +121,10 @@ export default {
               console.log("Error", error.message);
             }
           });
+      } else {
+        this.$toasted.error("You're Too Rich! Give More To Charity pls").goAway(3000)
+        this.transactionIsProcessing = false;
+        this.selectAmount(null);
       }
     }
   },
@@ -113,5 +149,16 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+  .otherAmount {
+    border-style: solid;
+    display: inline-block;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    width: 62px;
+    height: 32px;
+    border-color: #e0e0e0;
+    outline: none;
+}
 </style>

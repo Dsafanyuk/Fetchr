28e87afea0954ffd0f52e3d1c25a8e691dbecf4b
Dave Safanyuk <dsafanyuk@gmail.com>
2019-03-16 11:06:30 -0500

    Production sentry configuration set

--- api/app.js
+++ api/app.js
@@ -3,15 +3,16 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
 const apiRouter = require('./routes/index');
 
-const whitelist = [
-  'http://127.0.0.1:8080',
-  'https://fetchrapp.com',
-  'https://www.fetchrapp.com',
-];
-var corsOptions = {
-  origin: function(origin, callback) {
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({ dsn: 'https://a793d841eb444a7491f3bb6543e3957f@sentry.io/1411944' });
+}
+
+const whitelist = ['http://127.0.0.1:8080', 'https://fetchrapp.com', 'https://www.fetchrapp.com'];
+const corsOptions = {
+  origin(origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
@@ -35,12 +36,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors(corsOptions));
 
-
 app.use('/api', apiRouter);
 
 app.get('*', (req, res, next) => {
   const err = new Error('Page Not Found');
   err.statusCode = 404;
   next(err);
+});
+app.use(Sentry.Handlers.errorHandler());
+// Optional fallthrough error handler
+app.use((err, req, res, next) => {
+  res.statusCode = 500;
+  res.end(`${res.sentry}\n`);
 });
 module.exports = app;

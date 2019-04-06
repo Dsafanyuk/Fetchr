c3a39dd0153e31d60434aab5813762d6c7e5c93a
benji <bkintaudi@hotmail.com>
2019-04-05 21:15:41 -0500

    change the foote links and added carrot icon on the landing page

--- client/src/Components/layouts/MainDashboard/MainFooter.vue
+++ client/src/Components/layouts/MainDashboard/MainFooter.vue
@@ -1,20 +1,49 @@
 <template>
   <v-footer height="auto" color="primary">
     <v-layout justify-center row wrap>
-      <v-btn v-for="link in links" :key="link" color="white" flat round>{{ link }}</v-btn>
+      <v-btn v-for="link in links" :key="link" color="white" @click="footerActions(link)" flat round>{{ link }}</v-btn>
       <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>
         &copy;2018 —
         <strong>Fetchr</strong>
       </v-flex>
+      
     </v-layout>
   </v-footer>
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
   data: () => ({
-    links: ["Home", "About Us", "Team", "Contact Us"]
-  })
+    links: ["Home", "Courier", "Team", "Scroll up"]
+  }),
+  methods: {
+    footerActions: function(link){
+      switch(link){
+        case "Home": {
+          this.$router.push("/dashboard");
+          break;
+        }
+         case "Courier": {
+          this.$router.push("/courier");
+          break;
+        }
+        case "Team": {
+          this.$router.push("/home#team");
+          break;
+        }
+        case "Scroll up": {
+          this.scrollToTop();
+          break;
+        }
+          break;
+      }
+    },
+    scrollToTop: function() {
+      window.scrollTo(0, 0);
+    },
+  }
 };
 </script>
 

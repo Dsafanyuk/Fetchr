cadb6fd5656d6a965dadc85a2951f25b82cf6db4
jameslim0197 <jameslim0197@gmail.com>
2019-04-10 12:29:38 -0500

    remove console log

--- client/src/store/modules/notification.js
+++ client/src/store/modules/notification.js
@@ -9,7 +9,6 @@ Vue.use(Toasted);
 const state = {};
 const mutations = {
   NOTIFY_ACCEPTED: (state, message) => {
-    console.log(message.user == user);
     if ((message.user == user) && (router.history.current.fullPath != '/home')) {
       Vue.toasted.show(`Order #${message.order} has been accepted.`, {
         theme: 'bubble',
